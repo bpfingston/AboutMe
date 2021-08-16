65daf8cf3f57b4d1a4741044ed4ea894037c49e4
Bryce Pfingston <bryce.pfingston@gmail.com>
2021-08-15 23:53:39 -0600

    finished

--- Proj1/js/app.js
+++ Proj1/js/app.js
@@ -13,14 +13,11 @@ let whiteWater = prompt('Have I ever gone White Water Rafting?');
 let lowerWater = whiteWater.toLowerCase();
 
 if (lowerWater === 'yes'){
-    // console.log(good job!)
         alert('You are correct! I love White Water Rafting!')
         finalCount++;
 } else if ( lowerWater === 'no'){
-    // console.log(oh no!)
     alert('Wow, I thought you knew me! :(  I love WhiteWater Rafting!')
 } else {
-    // console.log(wut?)
     alert('Youre funny, but lets move on!')
 }
 }
@@ -32,14 +29,11 @@ let bigNursing = prompt('Have I ever worked in the medical field?');
 let nurse = bigNursing.toLowerCase(); 
 
 if (nurse === 'yes'){
-    // console.log(good job!)
     alert('You are correct! Did you know that nurses bring red magic markers into work? Its true, its so they can draw blood!')
     finalCount++;
 } else if (nurse === 'no'){
-    // console.log(oh no!)
     alert('I did unfortunately, I was a phlebotomist, and they made me go to Art school, so I would learn how to draw blood.')
 } else {
-    // console.log(wut?)
     alert('You are a riot, but lets move on!')
 }
 }
@@ -51,14 +45,11 @@ let psychDegree = prompt('Have I completed my degree in Psychology?');
 let degree = psychDegree.toLowerCase();
 
 if(degree === 'yes'){
-    // console.log(good job!)
     alert('You are correct, I have an Associates in psychology! Do you know the difference between a loan and psychologist? The loan eventually matures and earns money.')
     finalCount++;
 } else if( degree === 'no'){
-    // console.log(oh no!)
     alert('I do actually have a degree in psychology, I stopped when a paitent told me that no one understood them, and I asked what they meant?')
 } else {
-    // console.log(wut?)
     alert('Wow, I think you meant something else. Did you know that a Freudian Slip is when you say one thing and mean your mother?')
 }
 }
@@ -70,14 +61,11 @@ let jogGing = prompt('Have I ever competed in Triatholon?');
 let jog = jogGing.toLowerCase();
 
 if(jog === 'yes'){
-    // console.log(good job!)
     alert('No, unfortunately I have never ran in a triatholon, whenever I ran behind cars, I would become exhaust-ed.')
 } else if(jog === 'no'){
-    // console.log(oh no!)
     alert('You are correct, I did however run a half marathons. Do you know who invented running marathons? The Human Race.')
     finalCount++;
 } else {
-    // console.log(wut?)
     alert('Wow, thats really interesting, but do you know what they call a prank pulled by a coach on his track runner? A running gag.')
 }
 }
@@ -90,14 +78,11 @@ let cooKing = prompt('Am I an experienced Chef?');
 let cook = cooKing.toLowerCase();
 
 if(cook === 'yes'){
-    // console.log(good job!)
     alert('You would be wrong, but I do know that a good chef should never bring deviled eggs to church. ')
 } else if(cook === 'no'){
-    // console.log(oh no!)
     alert('I am not, but I like think that I can cook better than box macaroni. I was gonna tell a joke about cooking, but it just didnt pan out.')
     finalCount++;
 } else {
-    // console.log(wut?)
     alert('Coolio, but did you know that Harry Potter finds it extremely difficult to differentiate between a cooking utensil and his best friend because they are both cauld-ron.')
 }
 }
